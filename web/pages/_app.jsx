bf15ba02a2d825ac3966fc20e40a7db5e6184236
kason <kbraley9@gmail.com>
2022-01-19 23:17:12 -0700

    Adopt Next.js

---
+++ web/pages/_app.jsx
@@ -0,0 +1,29 @@
+import '../styles/globals.css';
+import '../styles/app.scss';
+import Layout from '../components/Layout.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+import SocketProvider from '../context/socket';
+import { Provider as StoreProvider } from 'react-redux';
+import store from '../store';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={typeof window !== "undefined" ? window.location.origin: ""}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <StoreProvider store={store()}>
+        <SocketProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SocketProvider>
+      </StoreProvider>
+    </Auth0Provider>
+  );
+}
+
+export default MyApp;
--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -2,12 +2,11 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import { useAuth0 } from '@auth0/auth0-react';
 
-import './App.scss';
-import Profile from './components/mainContainer/Profile';
-import Roomchat from './components/mainContainer/roomchat';
-import Matcher from './components/mainContainer/Matcher';
-import Landing from './components/mainContainer/Landing';
-import Layout from './components/Layout';
+import Profile from '../components/mainContainer/Profile';
+import Roomchat from '../components/mainContainer/roomchat';
+import Matcher from '../components/mainContainer/Matcher';
+import Landing from '../components/mainContainer/Landing';
+import Layout from '../components/Layout';
 
 function App() {
   let { isAuthenticated } = useAuth0();
@@ -18,6 +17,7 @@ function App() {
 
   return (
     <div className="App">
+    <Landing />
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<Layout />}>
